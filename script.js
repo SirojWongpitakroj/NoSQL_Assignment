3d65fd285b2a3af615934f36cd248366be77db41
Siroj Wongpitakroj <swongpitakroj24@gmail.com>
2026-04-03 23:53:41 +0700

    Added Remove Button and its Functionality. Completing the Recipe-Form Styling

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // add step
-const addStepBtn = document.getElementById('add-step-btn'); 
+const addStepBtn = document.getElementById('add-step-btn');
+
+const stepContainer = document.getElementById('steps-container');
 
 let stepCount = 1;
 
@@ -8,12 +10,14 @@ addStepBtn.addEventListener('click', () => {
         <input id="step_${stepCount}" type="textarea" name="step" placeholder="Enter Step" style="margin-top: 0.5rem;" required>
     `;
 
-    addStepBtn.insertAdjacentHTML("beforebegin", newStep);
+    stepContainer.insertAdjacentHTML("beforeend", newStep);
     stepCount++;
-})
+});
 
 // add equipment
 const addEqBtn = document.getElementById('add-equipment-btn');
+
+const equipmentContainer = document.getElementById('equipment-container');
 
 let eqCount = 1;
 
@@ -22,7 +26,7 @@ addEqBtn.addEventListener('click', () => {
         <input id="equipment_${eqCount}" type="text" name="equipment" placeholder="Enter Equipment Needed" style="margin-top: 0.5rem" required>
     `;
 
-    addEqBtn.insertAdjacentHTML("beforebegin", newEquipment);
+    equipmentContainer.insertAdjacentHTML("beforeend", newEquipment);
     eqCount++;
 });
 
@@ -78,5 +82,44 @@ let ingredientCount =  1;
         ingredientCount++;
  });
 
+//  Remove function
+const removeIngredientBtn = document.getElementById('ingredient-remove-btn');
+const removeStepBtn = document.getElementById('step-remove-btn');
+const removeEqBtn = document.getElementById('equipment-remove-btn');
+
+//Remove Ingredient
+removeIngredientBtn.addEventListener('click', () => {
+    
+    if (ingredientCount > 1) {
+        ingredientNameContainer.lastElementChild.remove();
+        ingredientAmountContainer.lastElementChild.remove();
+        ingredientUnitContainer.lastElementChild.remove();
+        ingredientCount--;
+    } else {
+        console.log("You must have at least 1 ingredient!")
+    }
+});
+
+//Remove Step
+removeStepBtn.addEventListener('click', () => {
+    
+    if (stepCount > 1) {
+        stepContainer.lastElementChild.remove();
+        stepCount--;
+    } else {
+        console.log("You must have at least 1 step!")
+    }
+});
+
+//Remove Equipment
+removeEqBtn.addEventListener('click', () => {
+    
+    if (eqCount > 1) {
+        equipmentContainer.lastElementChild.remove();
+        eqCount--;
+    } else {
+        console.log("You must have at least 1 equipment!")
+    }
+});
 
 
